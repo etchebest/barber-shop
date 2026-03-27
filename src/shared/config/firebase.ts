7ce0aec8f6a611d194a4894e3cf6926193a7b625
Cristian Ribeiro Etchebest <cristian.etchebest@gmail.com>
2026-03-27 17:48:20 -0300

    firstCommit

---
+++ src/shared/config/firebase.ts
@@ -0,0 +1,27 @@
+import admin from "firebase-admin";
+
+const isEmulator = process.env.NODE_ENV !== "production";
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    projectId: "barbershop-dev",
+  });
+}
+
+const firestore = admin.firestore();
+const auth = admin.auth();
+const rtdb = admin.database();
+const storage = admin.storage();
+
+if (isEmulator) {
+  firestore.settings({
+    host: "127.0.0.1:8080",
+    ssl: false,
+  });
+
+  process.env.FIREBASE_AUTH_EMULATOR_HOST = "127.0.0.1:9099";
+  process.env.FIREBASE_DATABASE_EMULATOR_HOST = "127.0.0.1:9000";
+  process.env.FIREBASE_STORAGE_EMULATOR_HOST = "127.0.0.1:9199";
+}
+
+export { firestore, auth, rtdb, storage };
